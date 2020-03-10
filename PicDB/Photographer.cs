b0fcb08ec1e0d8ca17ff957d2470eefa92b594ea
Simon Wiesinger <sime290498@yahoo.de>
2020-03-10 11:22:41 +0100

    Projektdateien hinzufügen.

---
+++ PicDB/Photographer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PicDB
+{
+    public class Photographer
+    {
+        public int ID { get; private set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime? Birthday { get; set; }
+        public List<Picture> Pictures { get; }
+
+    }
+}
